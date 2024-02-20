4f5137d7141dee942a75ad65f8051b6a3ab716e1
Sugyeong Sung <tjdtnrud9277@gmail.com>
2024-02-20 22:47:26 +0900

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY='fde08f1f8c894518a0d1aa99ddddfead'
 let news=[]
 const getLatestNews = async()=>{ //최신 뉴스 들고 오는 코드(함수를 담은 변수)
-    const url = new URL('http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?apiKey=http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?apiKey=${API_KEY}');  //url이란 인스턴스 생성
+    const url = new URL('https://sugyeong-times.netlify.app//top-headlines?apiKey=http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?apiKey=${API_KEY}');  //url이란 인스턴스 생성
     
     const response = await fetch(url);  //url 호츌
     const data = await response.json()  //data는 body에 있고 body에서 json(파일 형식 중 하나)으로 뽑아와야함
